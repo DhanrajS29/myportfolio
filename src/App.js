c8c7553ed4bafdd7af8b421eb50677d65b20ebc2
DhanrajS29 <dhanraj97863@gmail.com>
2024-02-10 09:25:44 +0530

    reactjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
+import { ThemeProvider } from 'styled-components';
 import './App.css';
+import { darkTheme, lightTheme } from "./utils/themes";
+import { Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HeroSection from './components/Herosection';
+import Skils from './components/Skills';
+import styled from 'styled-components';
+
+import { useState } from 'react';
+
+
+
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
+
+
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
+
 
 function App() {
+  const [darkMode, setdarkMode] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome home</h1>
-      </header>
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skils />
+          </Wrapper>
+
+        </Body>
+      </Router>
+
+    </ThemeProvider>
   );
 }
 
